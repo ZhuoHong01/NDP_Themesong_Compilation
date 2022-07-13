286e59162e6b322e2cb2ff1e18df65443046fbd4
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-13 11:54:58 +0800

    Commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/ndpthemesongcompilation/DBHelper.java
@@ -0,0 +1,48 @@
+package sg.edu.rp.c346.id21018193.ndpthemesongcompilation;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "songs.db";
+    private static final String TABLE_NOTE = "song";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_SINGERS = "singers";
+    private static final String COLUMN_YEARS = "years";
+    private static final String COLUMN_STARS = "stars";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String createNoteTableSql = "CREATE TABLE " + TABLE_NOTE + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT ) ";
+        sqLiteDatabase.execSQL(createNoteTableSql);
+
+        Log.i("info", "created tables");
+
+        //Dummy records, to be inserted when the database is created
+        for (int i = 0; i< 4; i++) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_TITLE, "Data number " + i);
+            sqLiteDatabase.insert(TABLE_NOTE, null, values);
+        }
+        Log.i("info", "dummy records inserted");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
