a0e24af778ed92ef3ae10ae0fd35b73a4c8ab3ea
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-13 14:46:34 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/ndpthemesongcompilation/ShowActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/ndpthemesongcompilation/ShowActivity.java
@@ -2,13 +2,52 @@ package sg.edu.rp.c346.id21018193.ndpthemesongcompilation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowActivity extends AppCompatActivity {
+    Button btnShow;
+    ArrayList<Song> al;
+    ListView lv;
+    ArrayAdapter<Song> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        btnShow = findViewById(R.id.btnShow);
+        lv = findViewById(R.id.lv);
+
+        al = new ArrayList<Song>();
+        aa = new ArrayAdapter<Song>(this,
+                android.R.layout.simple_list_item_1, al);
+        lv.setAdapter(aa);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long identity) {
+                Song data = al.get(position);
+                Intent i = new Intent(ShowActivity.this,
+                        MainActivity.class);
+                i.putExtra("data",  data);
+                startActivity(i);
+
+            }
+        });
     }
 }
