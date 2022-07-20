404154a27102d4a0b3671d40d3f36d22fcef94f3
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-20 12:22:42 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/ndpthemesongcompilation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/ndpthemesongcompilation/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Song target = al.get(0);
+                Song target = al.get(1);
 
                 Intent i = new Intent(MainActivity.this,
                         ShowActivity.class);
